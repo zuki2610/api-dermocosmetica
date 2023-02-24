0258d9ae111ed4f971241f0d7ee18318bfc76e01
Isabel Palacios <issabelpalacios@gmail.com>
2023-02-23 21:58:43 -0300

    FIX correccion en middleware aut

--- middleware/AuthMiddleware.js
+++ middleware/AuthMiddleware.js
@@ -2,12 +2,15 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 exports.requireAuth = (req, res, next) => {
-  const token = req.header('Authorization').split(' ')[1];
+  
+  //Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2M2Y4MDY4NDY4MzQ0Yzg0ZjM1YWFmOWIiLCJpYXQiOjE2NzcxOTkxMTcsImV4cCI6MTY3NzIwMTcwOX0.pSmseeLmyS8uz2eiI0IP-Yk8PPUFvZqgZAip0gknonk"
+  // operadorTernario = condicion ? verdadero : falso
+  const token =  req.headers.Authorization !== null && req.headers.Authorization !== undefined ? req.header('Authorization').split(' ')[1] : null;
   console.log('secret', process.env.JWT_SECRET);
   console.log(token);
   if (token) {
-    jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
-      if (err) {
+    jwt.verify(token, process.env.JWT_SECRET, (error, decodedToken) => {
+      if (error) {
         console.log(err);
         res.status(401).json({ error: 'Invalid token' });
       } else {
